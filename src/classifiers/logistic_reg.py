88773639ca19746e7964d0ed018c9170abbbe65d
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2022-05-25 21:35:50 +0200

    Project folders restructured. Added pulsar example folder. Added lda and gaussianization to preprocessing.

--- src/classifiers/logistic_reg.py
+++ src/classifiers/logistic_reg.py
@@ -1,43 +1,49 @@
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 
+from graphs import *
+
 
 # Logistic Regression binary classifier
-class LogisticReg:
-    def __init__(self, D, L):
-        self.D, self.L = D, L               # Training data and labels
+class LogisticReg(Classifier):
+    def __init__(self, l=10**-3, prior=None):
+        self.l = l
+        self.prior = prior
         self.w = None                       # Optimized weights vector, set in training
         self.b = None                       # Optimized biases vector, set in training
 
     # Loss function standard
-    def j_loss(self, v, l):
+    def j_loss(self, v, x, y):
         w_, b_ = v[0:-1], v[-1]
-        exps = np.array([np.dot(w_.T, self.D[:, i]) + b_ for i in range(self.D.shape[1])])
-        logY = np.log(1 + np.e ** (-(self.L * 2 - 1) * exps))
-        return (l * (w_ * w_).sum() / 2) + logY.mean()
+        exps = np.array([np.dot(w_.T, x[:, i]) + b_ for i in range(x.shape[1])])
+        logY = np.log(1 + np.e ** (-(y * 2 - 1) * exps))
+        return (self.l * (w_ * w_).sum() / 2) + logY.mean()
 
     # Loss function regularized with priors
-    def j_loss_reg(self, v, l, prior):
+    def j_loss_reg(self, v, x, y):
         w_, b_ = v[0:-1], v[-1]
-        exps = np.array([np.dot(w_.T, self.D[:, i]) + b_ for i in range(self.D.shape[1])])
-        logY = np.log1p(np.exp(-(self.L * 2 - 1) * exps))
-        logY_0_m = logY[self.L == 0].mean() * (1 - prior)
-        logY_1_m = logY[self.L == 1].mean() * prior
+        exps = np.array([np.dot(w_.T, x[:, i]) + b_ for i in range(x.shape[1])])
+        logY = np.log1p(np.exp(-(y * 2 - 1) * exps))
+        logY_0_m = logY[y == 0].mean() * (1 - self.prior)
+        logY_1_m = logY[y == 1].mean() * self.prior
 
-        return (l * (w_ * w_).sum() / 2) + logY_0_m + logY_1_m
+        return (self.l * (w_ * w_).sum() / 2) + logY_0_m + logY_1_m
 
     # Train binary logistic regression with lambda as hyperparameter
-    def train(self, l=0.0, prior=None):
-        x0 = np.zeros(self.D.shape[0] + 1)
+    def train(self, x, y):
+        x0 = np.zeros(x.shape[0] + 1)
 
         # Regularization requires prior not None
-        if prior is None:
-            x, value, _ = fmin_l_bfgs_b(self.j_loss, x0, args=[l], approx_grad=True, iprint=0)
+        if self.prior is None:
+            x, value, _ = fmin_l_bfgs_b(self.j_loss, x0, args=(x, y), approx_grad=True, iprint=0)
         else:
-            x, value, _ = fmin_l_bfgs_b(self.j_loss_reg, x0, args=(l, prior), approx_grad=True, iprint=0)
+            x, value, _ = fmin_l_bfgs_b(self.j_loss_reg, x0, args=(x, y), approx_grad=True, iprint=0)
 
         self.w, self.b = x[0:-1], x[-1]
 
     # Given an evaluation set returns the log likelihood ratio
-    def log_l_ratio(self, DTE):
-        return np.dot(self.w.T, DTE) + self.b
+    def transform(self, x):
+        return np.dot(self.w.T, x) + self.b
+
+    def __str__(self):
+        return f"LogReg(l:{self.l}, prior:{self.prior})"
--- src/examples/iris/logistic_reg_example.py
+++ src/examples/iris/logistic_reg_example.py
@@ -2,16 +2,16 @@ from examples.iris.data_utils import load_iris_binary_split
 from src.classifiers.logistic_reg import LogisticReg
 from preprocessing import *
 from plotting import *
+from measuring_predictions import *
 
 # Examples of training and test
 if __name__ == "__main__":
     (DTR, LTR), (DTE, LTE) = load_iris_binary_split()
 
-    LOG_REG = LogisticReg(DTR, LTR)
-
     for l in [0, 10**(-6), 10**(-3), 1.0]:
-        LOG_REG.train(l=l)
-        log_l = LOG_REG.log_l_ratio(DTE)
+        LOG_REG = LogisticReg(l)
+        LOG_REG.train(DTR, LTR)
+        log_l = LOG_REG.transform(DTE)
 
         print(f"LogReg (l:{l}) error rate:", str(((log_l > 0) != LTE).sum() / LTE.size * 100) + "%")
     print("==============================")
@@ -20,10 +20,10 @@ if __name__ == "__main__":
     DTR_PCA_2 = pca_2.fit_transform(DTR)
     DTE_PCA_2 = pca_2.transform(DTE)
 
-    LOG_REG = LogisticReg(DTR_PCA_2, LTR)
     for l in [0, 10**(-6), 10**(-3), 1.0]:
-        LOG_REG.train(l=l)
-        log_l = LOG_REG.log_l_ratio(DTE_PCA_2)
+        LOG_REG = LogisticReg(l=l)
+        LOG_REG.train(DTR_PCA_2, LTR)
+        log_l = LOG_REG.transform(DTE_PCA_2)
 
         print(f"LogReg (l:{l}, PCA: 2) error rate:", str(((log_l > 0) != LTE).sum() / LTE.size * 100) + "%")
     print("==============================")
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     DTR_LDA_2 = lda_2.fit_transform(DTR, LTR)
     DTE_LDA_2 = lda_2.transform(DTE)
 
-    LOG_REG = LogisticReg(DTR_LDA_2, LTR)
     for l in [0, 10**(-6), 10**(-3), 1.0]:
-        LOG_REG.train(l=l)
-        log_l = LOG_REG.log_l_ratio(DTE_LDA_2)
+        LOG_REG = LogisticReg(l=l)
+        LOG_REG.train(DTR_LDA_2, LTR)
+        log_l = LOG_REG.transform(DTE_LDA_2)
 
         print(f"LogReg (l:{l}, LDA: 2) error rate:", str(((log_l > 0) != LTE).sum() / LTE.size * 100) + "%")
